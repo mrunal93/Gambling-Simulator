ea489bdc9e2dc6131b54481ab2989be698d4e06b
mrunal93 <mrn26893@gmail.com>
2020-07-11 08:42:20 +0530

    [Mrunal]Add Wining or loosing condition

--- gambel.sh
+++ gambel.sh
@@ -3,3 +3,14 @@
 echo "Welcom to gambling"
 read -p "Enter the stake Amount:" stake
 read -p "Enter the Bet Amount:" betAmount
+
+winLoose() {
+	if [ $(( RANDOM%2 )) == 1 ]
+	then
+		echo "You Win"
+	else
+		echo "You Lose"
+	fi
+}
+
+winLoose
