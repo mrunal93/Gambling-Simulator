7a8db8835d97763aa003574b40268ce93ab6b10b
mrunal93 <mrn26893@gmail.com>
2020-07-11 09:06:56 +0530

    [Mrunal]ADD Resing For day 50%

--- gambel.sh
+++ gambel.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 echo "Welcom to gambling"
-read -p "Enter the stake Amount:" stake
-read -p "Enter the Bet Amount:" betAmount
-
-winLoose() {
-	if [ $(( RANDOM%2 )) == 1 ]
-	then
-		echo "You Win"
-	else
-		echo "You Lose"
-	fi
+#read -p "Enter the stake Amount:" stake
+#read -p "Enter the Bet Amount:" betAmount
+stake=100
+betAmount=1
+gambling() {
+	percentStake=$(($stake * 50 / 100))
+	minStake=$(($stake - $percentStake))
+	maxStake=$(($stake + $percentStake))
+	win=1
+	cash=$stake
+	while (( $cash > $minStake && $cash < $maxStake ))
+	do
+		if [ $(( RANDOM%2 )) -eq $win ]
+		then
+			echo "You Win"
+			cash=$(($cash + $betAmount))
+		else
+			echo "You Lose"
+			cash=$(($cash - $betAmount))
+		fi
+	done
+	echo $cash
 }
 
-winLoose
+gambling
