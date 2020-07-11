e77597e5e69cb99a16bf5a6f47e8aa23607d2444
mrunal93 <mrn26893@gmail.com>
2020-07-11 13:29:09 +0530

    [Mrunal]Add: calculate the cash for the Day

--- gambel.sh
+++ gambel.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 echo "Welcom to gambling"
-read -p "Enter the stake Amount:" stake
-read -p "Enter the Bet Amount:" betAmount
+stake=100
+betAmount=1
 
 winLoose() {
 	if [ $(( RANDOM%2 )) == 1 ]
 	then
 		echo "You Win"
+		cash=$(($stake + $betAmount))
 	else
 		echo "You Lose"
+		cash=$(($stake - $betAmount))
 	fi
+	echo $cash
 }
 
 winLoose
