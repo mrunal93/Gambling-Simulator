6bfdfe42d93b2ead43f3be42c98bf5983ad48196
mrunal93 <mrn26893@gmail.com>
2020-07-13 13:13:30 +0530

    [Mrunal]Refractor: proper Naming

--- gambel.sh
+++ gambel.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 echo "Welcom to gambling"
-#read -p "Enter the stake Amount:" stake
-#read -p "Enter the Bet Amount:" betAmount
-stake=100
-betAmount=1
+STAKE=100
+BET_AMOUNT=1
 gambling() {
 	winCash=0
 	lostCash=0
-	percentStake=$(($stake * 50 / 100))
-	minStake=$(($stake - $percentStake))
-	maxStake=$(($stake + $percentStake))
+	percentStake=$(($STAKE * 50 / 100))
+	minStake=$(($STAKE - $percentStake))
+	maxStake=$(($STAKE + $percentStake))
 	forWin=1
 	for (( day=1; day<=20; day++ ))
 	do
-		cash=$stake
+		cash=$STAKE
 		while (( $cash > $minStake && $cash < $maxStake ))
 		do
 			if [ $(( RANDOM%2 )) -eq $forWin ]
 			then
 				#echo "You Win"
-				cash=$(($cash + $betAmount))
+				cash=$(($cash + $BET_AMOUNT))
 			else
 				#echo "You Lose"
-				cash=$(($cash - $betAmount))
+				cash=$(($cash - $BET_AMOUNT))
 			fi
 		done
 
