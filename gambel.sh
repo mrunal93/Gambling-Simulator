fd57c94bc6f28ca26f35a747ed428919c12d6719
mrunal93 <mrn26893@gmail.com>
2020-07-13 13:24:33 +0530

    [Mrunal]Refractor: echo or print in single line

--- gambel.sh
+++ gambel.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
 echo "Welcom to gambling"
-#read -p "Enter the stake Amount:" stake
-#read -p "Enter the Bet Amount:" betAmount
-stake=100
-betAmount=1
+STAKE=100
+BET_AMOUNT=1
 declare -A gambler
 
 gambling() {
 	winCash=0
 	lostCash=0
-	percentStake=$(($stake * 50 / 100))
-	minStake=$(($stake - $percentStake))
-	maxStake=$(($stake + $percentStake))
+	percentStake=$(($STAKE * 50 / 100))
+	minStake=$(($STAKE - $percentStake))
+	maxStake=$(($STAKE + $percentStake))
 	forWin=1
-	for (( day=1; day<=20; day++ ))
+	for (( day=1; day<=30; day++ ))
 	do
-		cash=$stake
+		cash=$STAKE
 		while (( $cash > $minStake && $cash < $maxStake ))
 		do
 			if [ $(( RANDOM%2 )) -eq $forWin ]
 			then
-				echo "You Win"
-				cash=$(($cash + $betAmount))
+				cash=$(($cash + $BET_AMOUNT))
 			else
-				echo "You Lose"
-				cash=$(($cash - $betAmount))
+				cash=$(($cash - $BET_AMOUNT))
 			fi
 		done
-		gambler[$day]=$(( $cash - $stake))
+		gambler[$day]=$(( $cash - $STAKE))
 
 		if [ $cash -gt 100 ]
 		then
@@ -37,8 +33,7 @@ gambling() {
 			lostCash=$((lostCash + 50 ))
 		fi
 	done
-	echo -e "Total winning cash:$winCash \nTotal losing cash: $lostCash"
-	echo -e "Days won and lost: ${!gambler[@]} \nBy the Amount: ${gambler[@]}"
+	echo -e "Total winning cash:$winCash \nTotal losing cash: $lostCash \nDays won and lost: ${!gambler[@]} \nBy the Amount: ${gambler[@]}"
 	}
 
 gambling
