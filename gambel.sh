260249c7c73b261086fa1027fd847ca4c8cc4871
mrunal93 <mrn26893@gmail.com>
2020-07-13 13:04:29 +0530

    [Mrunal]Refractor: proper nameing

--- gambel.sh
+++ gambel.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Welcom to gambling"
-stake=100
-betAmount=1
+STAKE=100
+BET_AMOUNT=1
 
 winLoose() {
 	if [ $(( RANDOM%2 )) == 1 ]
 	then
 		echo "You Win"
-		cash=$(($stake + $betAmount))
+		cash=$(($STAKE + $BET_AMOUNT))
 	else
 		echo "You Lose"
-		cash=$(($stake - $betAmount))
+		cash=$(($STAKE - $BET_AMOUNT))
 	fi
 	echo $cash
 }
